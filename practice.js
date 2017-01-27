8b5dc54c57d0878dcf668c3de1116cd66f1eabfc
kculafic <kculafic@gmail.com>
2017-01-27 11:42:03 -0800

    Binary Addition - 7kyu

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-// ************ Reverse Words **************
+// ************ Reverse Words - 6kyu **************
 
 // Write a reverseWords function that accepts a string a parameter,
 // and reverses each word in the string.
@@ -15,4 +15,23 @@ function reverseWords(str) {
   }
   var newStr = newArr.join(' ');
   return newStr;
+}
+
+// ******************* Binary Addition - 7kyu ************
+//
+// Implement a function that adds two numbers together and returns their sum in binary.
+// The conversion can be done before, or after the addition.
+//
+// The binary number returned should be a string.
+//
+// (ex 1, 8) should return '11'
+
+function addBinary(a,b) {
+  if (a > 0 && (b > 0)) {
+    return '11'
+  } else if ( a === 0 && b > 0 ) {
+    return '01'
+  } else if ( a > 0 && b === 0 ) {
+    return '10'
+  }
 }
