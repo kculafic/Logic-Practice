4486cb9722f4e5f08a07fe4443281f441aad85bc
kculafic <kculafic@gmail.com>
2017-01-28 17:14:48 -0800

    IQ Test - 6kyu

--- practice.js
+++ practice.js
@@ -35,3 +35,58 @@ function addBinary(a,b) {
     return '10'
   }
 }
+
+
+// *************** IQ Test ********************
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+//
+// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+//
+// iqTest("2 4 7 8 10") => 3 ....Third number is odd, while the rest of the numbers are even
+// iqTest("1 2 1 1") => 2 ....Second number is even, while the rest of the numbers are odd
+
+
+function iqTest(numbers){
+  var nums = numbers.split(' ');
+  var newNums = nums.map(Number);
+  console.log(newNums);
+
+  debugger
+  var sum = 0;
+  for (i=0; i < newNums.length; i++) {
+     sum += newNums[i];
+  }
+  console.log('sum = ' + sum);
+  console.log('arrLength = ' + newNums.length)
+
+  // sum = odd
+  if (sum % 2 !== 0) {
+    for (j=0; j < newNums.length; j++) {
+      // trend = odd
+      if ( ((newNums[j+1] * newNums[j+1]) % 2 !== 0) ) {
+        for (k=0; k < newNums.length; k++) {
+          // length = even
+          if (newNums[k] % 2 === 0) {
+          console.log ('trend is odd')
+          return (k + 1)
+          }
+        }
+      } else if ( ((newNums[j] * newNums[j+1]) % 2 === 0) ) {
+        // trend = even
+        for (m=0; m < newNums.length; m++) {
+          if (newNums[m] % 2 !== 0) {
+          console.log ('trend is even')
+          return (m+1)
+          }
+        }
+      }
+    }
+  } else if (sum % 2 === 0) {
+    for (n=0; n < newNums.length; n++) {
+      if (newNums[n] % 2 === 0) {
+        console.log ('trend is odd')
+        return (n+1)
+      }
+    }
+  }
+}
