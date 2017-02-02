cf5b89087aef66e4aab1c39491afb920a523e09b
kculafic <kculafic@gmail.com>
2017-02-02 12:48:30 -0800

    Arithmetic - 7kyu

--- practice.js
+++ practice.js
@@ -115,3 +115,17 @@ function reverse(x) {
     return parseInt(join)
   }
 };
+
+//  *******************Arithmetic (7kyu) easy **************
+
+function arithmetic(a, b, operator){
+  if (operator === 'add') {
+    return a + b
+  } else if (operator === 'subtract') {
+    return a - b
+  } else if (operator === 'multiply') {
+    return a * b
+  } else if (operator === 'divide') {
+    return a / b
+  }
+}
