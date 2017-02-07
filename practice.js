8a0794ce7df5b1b82ad67c82ed76735b7cc9902a
kculafic <kculafic@gmail.com>
2017-02-06 18:33:20 -0800

    Factorial (Recursion) - 7 kyu

--- practice.js
+++ practice.js
@@ -243,3 +243,13 @@ function getCount(str) {
   }
   return vowelsCount;
 }
+
+// ********* Factorial (recursion) - 7 kyu ******
+var f = [];
+const factorial = n => {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+};
