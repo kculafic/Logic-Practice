9a60bd6f39190c95e29a311e1cdb48b9c1cc06a1
kculafic <kculafic@gmail.com>
2017-02-09 12:42:07 -0800

    Higher Order #1

---
+++ HigherOrderFunctionPractice.js
@@ -0,0 +1,14 @@
+// ***************** #1 Count JS Developers coming from Europe - 7kyu *********
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+
+function countDevelopers(list) {
+  var peeps = list.filter(function(i) {
+    return (i.continent === 'Europe' && i.language === 'JavaScript' )
+  })
+  return peeps.length
+}
