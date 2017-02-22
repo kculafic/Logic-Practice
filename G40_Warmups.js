069ab9c92d7cc8fdf3fc2900f250ab59e2cea41e
kculafic <kculafic@gmail.com>
2017-02-21 16:42:59 -0800

    g40 - warmup: factorial recursion

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -145,3 +145,22 @@ function doubleChar(str) {
     return "Ruh roh! That isn't a string!"
   }
 }
+
+// Create a function that takes a positive number and it returns its factorial.
+//
+// A factorial is the product of an number and all the integers below it;
+// e.g., factorial four ( 4! ) is equal to 24 (4*3*2*1).
+//
+// if the integer is zero, return 1; factorial(0) is defined as 1
+// if the integer is negative, return the string "undefined"
+
+function factorial(a){
+  var counter = a;
+  if (a === 0) {
+    return 1
+  } else if (a < 0) {
+    return 'undefined'
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
