d02af875e4ca42b45bc641db90f579b79a633dad
kculafic <kculafic@gmail.com>
2017-02-21 16:21:51 -0800

    g40 - warmup: Double char

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -115,3 +115,33 @@ function countVowels(str) {
   }
   return obj
 }
+
+
+
+// Given a string, you have to return a string in which
+// each character (case-sensitive) is repeated once.
+// if anything but a string is passed to the function,
+// return "Ruh roh! That isn't a string!"
+
+// doubleChar("String") ==> "SSttrriinngg"
+
+// doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+// doubleChar("1234!_ ") ==> "11223344!!__  "
+// Good Luck!
+
+// DONE?? Do it with a RegEx! (a 2 line solution is possible)
+
+function doubleChar(str) {
+  if (typeof str === 'string') {
+    var newArr = [];
+    for (let i=0; i < str.length; i++ ) {
+      newArr.push(str[i])
+      newArr.push(str[i])
+    }
+    var newStr = newArr.join('');
+    return newStr
+  } else {
+    return "Ruh roh! That isn't a string!"
+  }
+}
