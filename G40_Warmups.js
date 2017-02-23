8f83a06570aa3800aeaa3fe48093c30b3c26d482
kculafic <kculafic@gmail.com>
2017-02-22 18:52:25 -0800

    g40 - warmup: alphabet position

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -226,3 +226,27 @@ function removeDupesCI(input){
   }
   return newArr.join('')
 }
+
+
+// Welcome. In this warmup you are required to, given a
+// string, replace every letter with its position
+// in the alphabet. If anything in the text isn't a letter,
+// ignore it and don't return it. a being 1, b being 2,
+// etc. As an example:
+
+// alphabet_position("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20
+// 23 5 12 22 5 15 3 12 15 3 11" (As a string.)
+
+function alphabetPosition(text) {
+  var alphabetRef = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var newStr = text.toLowerCase();
+  var newArr = newStr.split('');
+  var nums = [];
+  for (i=0; i < newArr.length; i++) {
+    if (alphabetRef.indexOf(newArr[i]) !== -1) {
+      nums.push(alphabetRef.indexOf(newArr[i]))
+    }
+  }
+  return nums.join(' ')
+}
