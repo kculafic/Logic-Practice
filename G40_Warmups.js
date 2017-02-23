fef0d7a82152464fac574b7adc204d740f935bf4
kculafic <kculafic@gmail.com>
2017-02-23 15:09:36 -0800

    g40 - warmup: rotate array position

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -282,3 +282,17 @@ function reverseVowels(s) {
   }
   return newArr.join('')
 }
+
+
+// Rotate an array arr in place arr to the right by k steps.
+
+// For example, with n = [1,2,3,4,5,6,7] and k = 3, the array is rotated to [5,6,7,1,2,3,4].
+
+function rotate(arr,k) {
+  while (k > 0){
+    var x = arr.pop();
+    arr.unshift(x)
+    k--
+  }
+  return arr
+}
