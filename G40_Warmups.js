06365cea4d2208b8690064462fd1039edd1203a0
kculafic <kculafic@gmail.com>
2017-02-22 18:39:37 -0800

    g40 - warmup: removeDupes

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -190,3 +190,39 @@ function fizzbuzz(x){
   }
   return newArr
 }
+
+// Write a function named removeDupes which takes a string and removes
+// all duplicate characters in the String. Here are some examples:
+//   removeDupes('AABB'); // 'AB'
+//   removeDupes('AaAaBbBb'); // 'AaBb'
+//   removeDupes('cAtCaT'); // 'cAtCaT'
+
+// Write a function named removeDupesCI which takes a string and removes
+// all duplicate characters regardless of capitalization. Here are some examples:
+//   removeDupesCI('AABB'); // 'AB'
+//   removeDupesCI('AaAaBbBb'); // 'AB'
+//   removeDupesCI('cAtCaT'); // 'cAt'
+
+
+function removeDupes(input){
+  var newArr = [];
+  var Arr = input.split('');
+  for (i=0; i < Arr.length; i++) {
+    if (newArr.indexOf(Arr[i]) === -1)  {
+      newArr.push(Arr[i])
+    }
+  }
+  return newArr.join('')
+}
+
+//CI for case insensitive
+function removeDupesCI(input){
+  var newArr = [];
+  var Arr = input.split('');
+  for (i=0; i < Arr.length; i++) {
+    if (newArr.indexOf(Arr[i]) === -1 && newArr.indexOf(Arr[i].toUpperCase() ) === -1 && newArr.indexOf(Arr[i].toLowerCase() ) === -1)  {
+      newArr.push(Arr[i])
+    }
+  }
+  return newArr.join('')
+}
