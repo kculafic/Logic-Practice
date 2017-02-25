43a65d425e8d68a96217e4f77361f1be10f1c8fe
kculafic <kculafic@gmail.com>
2017-02-24 16:39:26 -0800

    g40 - warmup: find the sum of all the numbers inbetween two digits

--- G40_Warmups.js
+++ G40_Warmups.js
@@ -323,3 +323,38 @@ function namer(name){
   }
   return newArr.join('')
 }
+
+/* Given two integers, which can be positive and negative,
+find the sum of all the numbers between and including a and b,
+and return the sum. If both numbers are equal return a or b.
+Note! a and b are not ordered!
+Example:
+getSum(1, 0) == 1   // 1 + 0 = 1
+getSum(1, 2) == 3   // 1 + 2 = 3
+getSum(0, 1) == 1   // 0 + 1 = 1
+getSum(1, 1) == 1   // 1 Since both are same
+getSum(-1, 0) == -1 // -1 + 0 = -1
+getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+*/
+
+function getSum( a, b ) {
+  var arr = [];
+  if (a > b) {
+    while (b <= a) {
+      arr.push(b);
+      b++
+    }
+  } else  if (a < b) {
+    while (a <= b) {
+      arr.push(a);
+      a++
+    }
+  } else if (a === b) {
+    arr.push(a)
+  }
+  var counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    counter += arr[i]
+  }
+  return counter
+}
