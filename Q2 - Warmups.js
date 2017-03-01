7bb5985223e250260fb23980ce8f997306a7568b
kculafic <kculafic@gmail.com>
2017-03-01 13:26:43 -0800

    Q2 #8

--- Q2 - Warmups.js
+++ Q2 - Warmups.js
@@ -122,3 +122,27 @@ const tempGauge = function(towns){
   }
   return newObj
 }
+
+// ***************** # 8 ************
+
+// Require `data.js`, this module includes an array with the necessary information to process
+//
+// Create a function named `percentageUnder` that takes no parameters
+// this functions should return the percentage of accounts in the module that have
+// a balance below 0.
+//
+// The percentage should be a number between 0 and 1
+//
+// To test:
+// npm test 07-percentage-under/test.js
+
+var data = require('./data.js');
+
+const percentageUnder = function(){
+  let counter = 0;
+  let things = data.length;
+  for (let thing in data) {
+    if (data[thing].balance < 0) counter += 1;
+  }
+  return (counter/things);
+}
