af8fe227370a6f5616c44f2902185c6508a8b822
Krsto Culafic <kculafic@gmail.com>
2017-11-29 14:56:24 -0800

    Add reverseInt problem and solution

--- Udemy - Algorithm Coursework.js
+++ Udemy - Algorithm Coursework.js
@@ -26,21 +26,47 @@ function reverse(str) {
 // or false if it is not.  Palindromes are strings that
 // form the same word if it is reversed. *Do* include spaces
 // and punctuation in determining if the string is a palindrome.
-// --- Examples:   
+// --- Examples:
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
   let rev = str.split('').reverse().join('');
-
   if (str === rev) {
     return true;
   } else {
     return false;
   }
 }
+// *** Alternate solution
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i -1];
+  })
+}
 
-//
+//  ************************* 3 ******************************
+
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  let sign = Math.sign(n);
+  let x = n.toString().split('').reverse().join('');
+  return sign * parseInt(x)
+}
+
+
+
+
+
 
 // --- Directions
 // Check to see if two provided strings are anagrams of eachother.
