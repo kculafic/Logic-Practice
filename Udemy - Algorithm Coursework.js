9180ec4304182b1497aa4c95462d4cdd04df9860
Krsto Culafic <kculafic@gmail.com>
2018-01-03 13:29:36 -0800

    Add recursive solution to steps problem

--- Udemy - Algorithm Coursework.js
+++ Udemy - Algorithm Coursework.js
@@ -220,3 +220,71 @@ function capitalize(str) {
   }
   return result.join('')
 }
+// ******************* 9 **********************************
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+// initial solution
+function steps(n) {
+  const shape = [];
+  for (i=0; i<n; i++){
+    shape.push(' ');
+  }
+  for (j=0; j<n; j++) {
+    shape[j] = '#';
+    console.log(shape.join(''));
+  }
+}
+
+// alternate solution
+function steps(n) {
+  for (row=0; row < n; row++) {
+    let shape = '';
+
+    for (column=0; column < n; column++) {
+      if (column <= row ) {
+        shape += '#';
+      } else {
+        shape += ' ';
+      }
+    }
+
+    console.log(shape);
+  }
+}
+
+// Recursive solution
+function steps(n, row = 0, stair='') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+   stair += '#';
+  } else {
+    stair += ' ';
+  }
+  
+  steps(n, row, stair);
+}
