f9801723134f0347c4814ff1de02fde34c9abdca
Krsto Culafic <kculafic@gmail.com>
2018-01-04 15:46:51 -0800

    Add vowels problem & solution

--- Udemy - Algorithm Coursework.js
+++ Udemy - Algorithm Coursework.js
@@ -370,4 +370,26 @@ function pyramid(n, row = 0, level = '') {
     return pyramid(n, row + 1);
   }
 }
-  
+
+
+// *********************** 11 ********************
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+  let counter = 0;
+  let newStr = str.toLowerCase();
+
+  for (i=0; i<newStr.length; i++) {
+    if (newStr[i] === 'a' || newStr[i] === 'e' || newStr[i] === 'i' || newStr[i] === 'o' || newStr[i] === 'u') {
+      counter ++;
+    }
+  }
+ return counter;
+}
