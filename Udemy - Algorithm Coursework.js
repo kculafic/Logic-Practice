1b78813fe24828f212b90e5ed92010fe36ae1dfa
Krsto Culafic <kculafic@gmail.com>
2018-01-09 15:50:50 -0800

    Add Fibonacci problem, iterative solution

--- Udemy - Algorithm Coursework.js
+++ Udemy - Algorithm Coursework.js
@@ -466,3 +466,30 @@ function matrix(n) {
   }
   return results;
 }
+
+// ********************** 13 **************************
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+  let arr = [];
+  for (let i=0; i <= n; i++) {
+    if (i === 0) {
+      arr.push(0);
+    } else if (i === 1) {
+      arr.push(1);
+    } else if (i >= 2) {
+      let first = arr[i-2];
+      let second = arr[i-1];
+      arr.push(first+second);
+    }
+  }
+  return arr[n];
+}
