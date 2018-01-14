500543e70e5426607ae80e9679efe58671c09866
Krsto Culafic <kculafic@gmail.com>
2018-01-13 16:29:31 -0800

    Add `queue from two stacks` problem and solution

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -79,3 +79,99 @@ function weave(sourceOne, sourceTwo) {
   }
   return q
 }
+
+// ***************** 3 - Create a Stack ********************
+
+// --- Directions
+// Create a stack data structure.  The stack
+// should be a class with methods 'push', 'pop', and
+// 'peek'.  Adding an element to the stack should
+// store it until it is removed.
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(input) {
+    this.data.push(input);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    const last = this.data.length - 1;
+    return this.data[last];
+  }
+}
+
+// ***************** 3 - Create a Queue using two stacks ********************
+
+// --- Directions
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+// const Stack = require('./stack');
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+  push(record) {
+    this.data.push(record);
+  }
+  pop() {
+    return this.data.pop();
+  }
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+  add(input) {
+    this.first.push(input);
+  }
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.pop();
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.peek();
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+}
