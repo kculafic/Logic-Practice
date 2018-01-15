74f9eeb03d420400b266fd6032fc846302e223b0
Krsto Culafic <kculafic@gmail.com>
2018-01-15 12:32:31 -0800

    Add Linked List Problem w/ constructors

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -113,7 +113,7 @@ class Stack {
   }
 }
 
-// ***************** 3 - Create a Queue using two stacks ********************
+// ***************** 4 - Create a Queue using two stacks ********************
 
 // --- Directions
 // Implement a Queue datastructure using two stacks.
@@ -150,28 +150,5 @@ class Queue {
   constructor() {
     this.first = new Stack();
     this.second = new Stack();
-  }
-  add(input) {
-    this.first.push(input);
-  }
-  remove() {
-    while (this.first.peek()) {
-      this.second.push(this.first.pop());
-    }
-    const record = this.second.pop();
-    while (this.second.peek()) {
-      this.first.push(this.second.pop());
-    }
-    return record;
-  }
-  peek() {
-    while (this.first.peek()) {
-      this.second.push(this.first.pop());
-    }
-    const record = this.second.peek();
-    while (this.second.peek()) {
-      this.first.push(this.second.pop());
-    }
-    return record;
   }
 }
