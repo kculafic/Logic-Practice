3927fa51c857aa187a3b3443d8b6465e11badccd
Krsto Culafic <kculafic@gmail.com>
2018-01-15 12:35:48 -0800

    Add some methods to Linked List class

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -151,4 +151,66 @@ class Queue {
     this.first = new Stack();
     this.second = new Stack();
   }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size(){
+    let count = 0;
+    let node = this.head;
+
+    while (node !== null) {
+     count++;
+     node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+   let node = this.head;
+   if (!node) {
+     return null
+   }
+
+   while (node) {
+     if (node.next) {
+       node = node.next;
+     } else {
+       return node
+     }
+   }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return
+    }
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
 }
