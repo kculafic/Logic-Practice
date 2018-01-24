b10ca0af3d27d9592fdeb6f8fc9ec398e84bd9a8
Krsto Culafic <kculafic@gmail.com>
2018-01-24 14:35:45 -0800

    Add solution to `Find Midpoint of a LinkedList` problem

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -302,3 +302,33 @@ class LinkedList {
     previous.next = previous.next.next;
   }
 }
+
+// *************** Find the midpoint of a LinkedList ***********
+// --- Directions
+// Return the 'middle' node of a linked list.
+// If the list has an even number of elements, return
+// the node at the end of the first half of the list.
+// *Do not* use a counter variable, *do not* retrieve
+// the size of the list, and only iterate
+// through the list one time.
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast('a')
+//   l.insertLast('b')
+//   l.insertLast('c')
+//   midpoint(l); // returns { data: 'b' }
+
+const Node = require('./linkedlist');
+const LinkedList = require('./linkedlist');
+
+function midpoint(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+  }
+  return slow;
+}
