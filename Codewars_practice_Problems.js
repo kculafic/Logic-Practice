9948888af4d006555ba2856d59db1c718d977b7c
Krsto Culafic <kculafic@gmail.com>
2018-02-01 13:59:05 -0800

    6 kyu: Highest Scoring Word

--- Codewars_practice_Problems.js
+++ Codewars_practice_Problems.js
@@ -475,3 +475,41 @@ function balance(left,right){
     return "Right"
   }
 }
+
+// ********************* 6 kyu: Highest Scoring Word *******************
+// Given a string of words, you need to find the highest scoring word.
+//
+// Each letter of a word scores points according to
+// it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+//
+// You need to return the highest scoring word as a string.
+//
+// If two words score the same, return the word
+// that appears earliest in the original string.
+//
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+  let arr = x.toLowerCase().split(' ');
+  let alphaRef = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,  'j': 10,  'k': 11,  'l': 12,  'm': 13,  'n': 14,  'o': 15,  'p': 16, 'q': 17,   'r': 18,   's': 19,  't': 20,  'u': 21,  'v': 22,  'w': 23,  'x': 24,  'y': 25,  'z': 26 };
+  let counterArr = [];
+
+  for (i=0; i<arr.length; i++) {
+    let count = 0;
+    for (j=0; j<arr[i].length; j++) {
+      count += alphaRef[arr[i][j]];
+    } 
+    counterArr.push(count);
+  }
+
+  let maxCount = 0;
+  let maxIndex = 0;
+
+  for (i=0; i < counterArr.length; i++) {
+    if (counterArr[i] > maxCount) {
+      maxCount = counterArr[i];
+      maxIndex = i;
+    }
+  }
+  return arr[maxIndex]
+}
