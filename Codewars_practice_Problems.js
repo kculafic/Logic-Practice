eaf2309bc9e8b3cbfe39dda0a7738e919fc2fe19
Krsto Culafic <kculafic@gmail.com>
2018-02-01 13:27:58 -0800

    6kyu Problem - Balance !s and ?s

--- Codewars_practice_Problems.js
+++ Codewars_practice_Problems.js
@@ -437,3 +437,41 @@ function mostUsedChar(str) {
 }
 
 threesAndFives(1000);
+
+// ********************* 6 kyu: Balance !'s and ?'s *******************
+// Each exclamation mark weight is 2; Each question mark weight is 3.
+// Put two string left and right to the balance, Are they balanced?
+//
+// If the left side is more heavy, return "Left";
+// If the right side is more heavy, return "Right";
+// If they are balanced, return "Balance".
+function balance(left,right){
+  let leftCount = 0;
+  let rightCount = 0;
+
+  for (i=0; i < left.length; i++) {
+    if (left[i] === '!') {
+      leftCount += 2;
+    }
+    if (left[i] === '?') {
+      leftCount += 3;
+    }
+  }
+
+  for (i=0; i < right.length; i++){
+    if (right[i] === '!') {
+      rightCount += 2;
+    }
+    if (right[i] === '?') {
+      rightCount += 3;
+    }
+  }
+
+  if (leftCount === rightCount) {
+    return "Balance"
+  } else if (leftCount > rightCount) {
+    return "Left"
+  } else if (rightCount > leftCount) {
+    return "Right"
+  }
+}
