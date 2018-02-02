adf0cf991a9a04703d256ac70051f5ed638dbdbe
Krsto Culafic <kculafic@gmail.com>
2018-02-01 16:58:09 -0800

    Refactor to optimize alphabet reference obj, add notes

--- Codewars_practice_Problems.js
+++ Codewars_practice_Problems.js
@@ -490,18 +490,29 @@ function balance(left,right){
 // All letters will be lowercase and all inputs will be valid.
 
 function high(x){
+  let alphaRef = {};
+  let alphaStr = 'abcdefghijklmnopqrstuvwxyz';
+  let values = 1;
+
+  // Iterate through alphabet to populate alphabet reference obj
+  for (i = 0; i < alphaStr.length; i++) {
+    alphaRef[alphaStr[i]] = values;
+    values++;
+  }
+
+  // Add logic to iterate through letters of each word, to populate counterArr
   let arr = x.toLowerCase().split(' ');
-  let alphaRef = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,  'j': 10,  'k': 11,  'l': 12,  'm': 13,  'n': 14,  'o': 15,  'p': 16, 'q': 17,   'r': 18,   's': 19,  't': 20,  'u': 21,  'v': 22,  'w': 23,  'x': 24,  'y': 25,  'z': 26 };
   let counterArr = [];
 
   for (i=0; i<arr.length; i++) {
     let count = 0;
     for (j=0; j<arr[i].length; j++) {
       count += alphaRef[arr[i][j]];
-    } 
+    }
     counterArr.push(count);
   }
 
+  // Iterate to compare point values, return final answer
   let maxCount = 0;
   let maxIndex = 0;
 
