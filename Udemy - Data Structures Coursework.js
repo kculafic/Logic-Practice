bd2a80782d60e3358ce2179f1d40e0f9659ddd39
Krsto Culafic <kculafic@gmail.com>
2018-02-02 16:25:31 -0800

    Add Depth First Traversal method to Tree constructor

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -440,4 +440,14 @@ class Tree {
       fn(node);
     }
   }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }   
 }
