246cc2c07aaca21cda8ad8fbbd6e9d3ea1e742af
Krsto Culafic <kculafic@gmail.com>
2018-02-02 17:02:49 -0800

    Tree Traversal: Level Width Problem

--- Udemy - Data Structures Coursework.js
+++ Udemy - Data Structures Coursework.js
@@ -449,5 +449,49 @@ class Tree {
       arr.unshift(...node.children);
       fn(node);
     }
-  }   
+  }
+}
+
+// ************** Tree Traversal: Level Width Problem ************
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+
+// ie: how many nodes present at each level, returned in an array
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+}
+
+function levelWidth(root) {
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters
 }
